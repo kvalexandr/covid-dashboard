74229a4f074d17a32d014b767948e564b6db94f1
Alexandr <k.v.alexandr@gmail.com>
2020-12-22 20:42:53 +0400

    refactor: virtual-keyboard

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../sass/index.scss';
-//import './core/virtual-keyboard/script';
+import './core/virtual-keyboard/script';
 
 import tableController from './controllers/tableController';
 import countryController from './controllers/countryController';
