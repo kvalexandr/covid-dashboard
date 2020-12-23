722e3ff9b9607dd8e9fa1415ac933639bac688be
Alexandr <k.v.alexandr@gmail.com>
2020-12-23 04:01:18 +0400

    feat: favicon

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,10 @@ module.exports = {
     }),
     new CopyPlugin([
       {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist'),
+      },
+      {
         from: path.resolve(__dirname, 'src/assets'),
         to: path.resolve(__dirname, 'dist/assets'),
       },
